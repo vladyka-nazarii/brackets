0b720a21a19de5c6e9bb369bf9ba6de8faa77bac
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2022-07-27 22:55:29 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = [];
+  const duplicate = [];
+  let order = true;
+  bracketsConfig.flat().forEach((element, index, array) => {if (array[index] === array[[index + 1]]) {duplicate.push(element)}});
+  str.split('').forEach(element => {
+    if (duplicate.includes(element)) {if (brackets[brackets.length-1] === element) {brackets.pop()} else {brackets.push(element)}}
+    else if (bracketsConfig.flat().findIndex((e) => {return e === element}) % 2 === 0) {brackets.push(element)}
+    else if (bracketsConfig.flat().findIndex((e) => {return e === element}) -
+            bracketsConfig.flat().findIndex((e) => {return e === brackets[brackets.length-1]}) === 1) {brackets.pop()}
+    else {order = false}});
+  return (brackets.join('') === '' && order);
 }
